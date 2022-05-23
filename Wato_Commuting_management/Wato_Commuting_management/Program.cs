21c8501c90afd45765ba7cf3e9d8e74b6a0235bb
JAEGYU2002 <jk.lee@watosolution.com>
2022-05-23 20:03:10 +0900

    회사 IP대역 외 다른 IP로도 실행할 수 있도록 수정

--- Wato_Commuting_management/Wato_Commuting_management/Program.cs
+++ Wato_Commuting_management/Wato_Commuting_management/Program.cs
@@ -16,20 +16,26 @@ namespace Wato_Commuting_management
         static void Main()
         {
             log.Info("=============  Started Client application  =============");
-            
-            if (GetExternalIPAddress() != "121.137.90.73")
-            {
-                MessageBox.Show("와토솔루션 본사에서만 사용하실 수 있습니다.", "접근 거부 알림", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return;
-            }
-            else
-            {
-                DB_info_xml.DB_info_xml_read();
 
-                Application.EnableVisualStyles();
-                Application.SetCompatibleTextRenderingDefault(false);
-                Application.Run(new Login());
-            }
+            DB_info_xml.DB_info_xml_read();
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Login());
+
+            //if (GetExternalIPAddress() != "121.137.90.73")
+            //{
+            //    MessageBox.Show("와토솔루션 본사에서만 사용하실 수 있습니다.", "접근 거부 알림", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //    return;
+            //}
+            //else
+            //{
+            //    DB_info_xml.DB_info_xml_read();
+
+            //    Application.EnableVisualStyles();
+            //    Application.SetCompatibleTextRenderingDefault(false);
+            //    Application.Run(new Login());
+            //}
         }
 
         public static string GetExternalIPAddress()
